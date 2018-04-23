8034b0535f0d56abf2b99d705f6a0c92dfd5b6e4
Pavel <pavel@test.int>
2018-04-23 16:54:22 +0300

    search in elastic

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from elasticsearch import Elasticsearch
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+es = Elasticsearch([{'host': '192.168.7.5', 'port': 9200}])
 
-import requests
-res = requests.get('http://localhost:9200')
-print(res.content)
+#print(es.info())
+
+#print(es.get(index="filebeat-2018.04.23", doc_type="doc", id='uC-y8mIBAt7Av6H3m_mc'))
+a = es.search(index='filebeat-2018.04.23', body={"query": {"prefix" : { "message" : "pass" }}})
+
+
+# for k, v in a:
+#     if k == 'system message':
+#         print(v)
+
+#import requests
+#res = requests.get('http://192.168.7.5:9200')
+#print(res.content)
