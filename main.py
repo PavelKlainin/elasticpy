5230eee3727c4b51f1434c8124eb6e53c871dc00
Pavel <pavel@test.int>
2018-05-07 22:18:28 +0300

    find ip from log

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 from elasticsearch import Elasticsearch
-#import datetime
+import sys
 from datetime import datetime
+import re
 
-es = Elasticsearch([{'host': '192.168.7.5', 'port': 9200}])
+
+try:
+    es = Elasticsearch([{'host': '192.168.7.5', 'port': 9200}])
+    print('Connected', es.info())
+except Exception as ex:
+    print('Error: ', ex)
+    sys.exit()
+
 
 #def es_query:
-#print(es.info())
-#print(datetime.datetime.now())
+
 
 today = datetime.strftime(datetime.now(), 'filebeat-%Y.%m.%d')
-#print(today)
 
-res = es.search(index=today, body={"query": {"prefix": {"message": "sshd"}}})
+res = es.search(index=today, body={'query': {'prefix': {'message': 'sshd'}}})
 
-print("{} documents found".format(res['hits']['total']))
+#res = es.search(index=today, body={'query': {'prefix': )
+
+print('{} documents found'.format(res['hits']['total']))
+#print(res['hits']['hits'])
+
+regex_ip = re.compile(r'([0-9]{1,3}\.){3}[0-9]{1,3}')
 
 for doc in res['hits']['hits']:
     if 'Failed' in doc['_source']['message']:
-        print("{} --- {}".format(doc['_id'], doc['_source']['message']))
+        ip = regex_ip.search(doc['_source']['message']).group()
+        print(ip)
+        #print("At Host {} --- {}".format(doc['_source']['beat']['hostname'], doc['_source']['message']))
+        #if regex_ip in
 
