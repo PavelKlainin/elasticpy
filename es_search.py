1b10f875ab6160f72676088da77b7d14aa45ccc0
Pavel <pavel@test.int>
2018-05-10 10:23:54 +0300

    send message working

--- es_search.py
+++ es_search.py
@@ -2,35 +2,33 @@ from datetime import datetime
 import re
 from connector import es_connect
 
-es = es_connect()
+es = es_connect() # connecting to Elasticsearch
+
 
 def es_search():
-    today = datetime.strftime(datetime.now(), 'filebeat-%Y.%m.%d')
-    # res = es.search(index=today, body={'query': {'prefix': {'message': 'sshd'}}})
-    #res = es.search(index=today, body={'query': {'match': {'message': 'Failed passwordsdfsdfsdfsdfsdfsdfsdfsdf'}}})
-    res = es.search(index=today, body={'query': {'match': {'message': 'dfsdfsdfsdfsdfsdfsdfsdf'}}})
-    # res = es.search(index=today, body={"query": {"match_all": {}}})
-    # res = es.search()
-    # print(res)
-
-    # print('{} documents found'.format(res['hits']['total']))
+    today = datetime.strftime(datetime.now(), 'filebeat-%Y.%m.%d')  # format index for today
+    res = es.search(index=today, body={'query': {'match': {'message': 'Failed password'}}})
+    # res = es.search(index=today, body={'query': {'match': {'message': 'dfsdfsdfsdfsdfsdfsdfsdf'}}})   # testing empty result
+    # print('{} documents found'.format(res['hits']['total']))  # how many documents found
     # print(res['hits']['hits'])
 
-    regex_ip = re.compile(r'([0-9]{1,3}\.){3}[0-9]{1,3}')
+    regex_ip = re.compile(r'([0-9]{1,3}\.){3}[0-9]{1,3}')   # regex for find IPv4
 
-    ip_who_attack = {}
-
-    for doc in res['hits']['hits']:
-        if 'sshd' in doc['_source']['message']:
-            ip = regex_ip.search(doc['_source']['message']).group()
-            if ip in ip_who_attack:
-                ip_who_attack.update({ip: ip_who_attack[ip] + 1})
+    ip_who_attack = {}  # dict for founded ip addresses
+    if res['hits']['total'] > 0:
+        for doc in res['hits']['hits']:
+            if 'sshd' in doc['_source']['message']:
+                ip = regex_ip.search(doc['_source']['message']).group()
+                if ip in ip_who_attack:
+                    ip_who_attack.update({ip: ip_who_attack[ip] + 1})
+                else:
+                    ip_who_attack.update({ip: 1})
             else:
-                ip_who_attack.update({ip: 1})
-        else:
-            return None
-    #print(ip_who_attack)
+                #print('Today there was no attack on ssh.')
+                return False    # there are no 'sshd' in log
+    else:
+        print('Today there was no attack on ssh.')
+        return False    # no search result
     return ip_who_attack
     # print(ip, doc['_source']['@timestamp'])
     # print("At Host {} --- {}".format(doc['_source']['beat']['hostname'], doc['_source']['message']))
-    # if regex_ip in
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from es_search import es_search
 from send_message import telegram
+import subprocess
+
+if es_search():
+    ip_dict = es_search()
+    print(ip_dict)
+    for ip, quantity in ip_dict.items():
+        if quantity >= 4:
+            cmd = 'echo iptables -A INPUT -s {} -p tcp --dport 22 -j DROP'.format(ip)
+            print(cmd)
+            #subprocess.call(cmd)
 
 
-ip = es_search()
-print(ip)
-message = 'Banned IP: {}'.format(ip)
 
-telegram(message)
+    message = 'Banned IP: {}'.format(ip)
+
+    telegram(message)
 
