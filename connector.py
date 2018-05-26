4ff8b91d43038c89b26db13080214b5d63a487e8
a-i <pavel.v.kalinin@gmail.com>
2018-05-26 22:06:52 +0300

    change ip ES

--- connector.py
+++ connector.py
@@ -4,8 +4,8 @@ import sys
 
 def es_connect():
     try:
-        #es = ES([{'host': '192.168.56.101', 'port': 9200}])
-        es = ES([{'host': '127.0.0.1', 'port': 9200}])
+        es = ES([{'host': '192.168.56.101', 'port': 9200}])
+        #es = ES([{'host': '127.0.0.1', 'port': 9200}])
         # print('Connected', es.info())
         return es
     except Exception as ex:
